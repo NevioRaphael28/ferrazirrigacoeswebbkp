351298bf1441240d698e71f25c7883fd8bd27d88
gabriel.olandim <gabriel@landim.dev>
2024-09-12 21:58:00 -0300

    Alterações no backend caixa

--- FerrazIrrigacoes/Controllers/HomeController.cs
+++ FerrazIrrigacoes/Controllers/HomeController.cs
@@ -1,20 +1,54 @@
-﻿using System;
+﻿using FerrazIrrigacoes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace FerrazIrrigacoes.Controllers
+namespace ferrazIrrigacoesWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private sakitadbEntities db = new sakitadbEntities();
         public ActionResult Index()
         {
-            if (Session["Usuarioid"] == null)
+            if (Session["UsuarioId"] == null)
             {
                 return RedirectToAction("Login", "Usuario", null);
             }
+
+            var caixa = db.Caixa.Where(registro => registro.DataFechamento.Equals(null)).FirstOrDefault();
+
+            if (caixa == null)
+            {
+                Session["CaixaId"] = "Fechado";
+            }
+            else
+            {
+                Session["CaixaId"] = caixa.Id;
+                Session["SaldoCaixa"] = db.CalculaCaixa(Convert.ToInt32(Session["CaixaId"])).FirstOrDefault();
+            }
+
             return View();
+        }
+
+        [HttpGet]
+        public JsonResult BuscaSaldoCaixa(int id)
+        {
+            if (Session["CaixaId"].ToString() != "Fechado")
+            {
+                var saldo = db.CalculaCaixa(Convert.ToInt32(Session["CaixaId"])).FirstOrDefault();
+                int caixaid = Convert.ToInt32(Session["CaixaId"]);
+                var caixa = db.Caixa.Where(c => c.Id == caixaid).FirstOrDefault();
+                double saldototal = Convert.ToDouble(saldo.Value) + Convert.ToDouble(caixa.TotalInicial);
+
+                return Json(new { sucesso = true, valor = saldototal }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { sucesso = true, valor = 0 }, JsonRequestBehavior.AllowGet);
+            }
+
         }
 
         public ActionResult About()
